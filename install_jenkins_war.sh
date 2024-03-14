0ef5225308fbc19752960dcf962cf49b8e85a0a2
Ashish Kumar <kashish979@gmail.com>
2024-03-14 13:39:29 +0530

    Update install_jenkins_war.sh

--- install_jenkins_war.sh
+++ install_jenkins_war.sh
@@ -1,8 +1,8 @@
 #Install Java 11 and Later
-apt update && apt install openjdk-11-jdk
+apt update && apt install openjdk-21-jdk -y
 
 #Download Jenkins war files
 wget https://get.jenkins.io/war-stable/2.440.1/jenkins.war
 
 #Running Jenkins
-java -jar jenkins.war --httpPort=8080
+java -jar jenkins.war -Dhudson.security.csrf.GlobalCrumbIssuerConfiguration.DISABLE_CSRF_PROTECTION=true --httpPort=8080
